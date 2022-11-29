b7826dea649d934ecb72c15f214f54603546827e
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2022-11-29 11:15:25 +0700

    add func to check connect to db

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/',function(){
+    try {
+        DB::connection()->getPdo();
+        if(DB::connection()->getDatabaseName()){
+            echo "Yes! Successfully connected to the DB: " . DB::connection()->getDatabaseName();
+        }else{
+            die("Could not find the database. Please check your configuration.");
+        }
+    } catch (\Exception $e) {
+        die("Could not open connection to database server.  Please check your configuration.");
+    }
+});
+
 // Card routes
 Route::controller(Cart::class)
     ->prefix('cart')
